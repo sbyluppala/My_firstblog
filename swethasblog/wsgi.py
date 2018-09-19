2a70ffdeb637ac48a983f043998175a9aff4b4d7
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-19 16:35:22 -0500

    updated wsgi file

--- swethasblog/wsgi.py
+++ swethasblog/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swethasblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Asgnm_Proj1.settings')
 
 application = get_wsgi_application()
 
