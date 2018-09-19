0df63a01c00cbf237a1613ea807f584f8ed99e16
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-19 11:08:44 -0500

    First commit to GitHub

---
+++ swethasblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SwethasblogConfig(AppConfig):
+    name = 'swethasblog'
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Prithvi/Documents/Python/week4/Asgnm_Proj1/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
