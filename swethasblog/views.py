dbab51c9c701531e7da9712fc901992e647281ef
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-19 13:17:11 -0500

    Changes to views

--- swethasblog/views.py
+++ swethasblog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-
 
 # Create your views here. publish
 
 
 def post_list(request):
-    posts = Post.objects.filter(publish=timezone.now()).order_by('publish')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
