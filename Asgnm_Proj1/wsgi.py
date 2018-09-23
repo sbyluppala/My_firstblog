5cde9d6c6d468f26c2e2a10e822c0d639f7c6e2e
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-22 20:55:44 -0500

    modifications in wsgi and settings file to elimnate error of "error importing files"

--- Asgnm_Proj1/wsgi.py
+++ Asgnm_Proj1/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Asgnm_Proj1.settings')
+from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
+
