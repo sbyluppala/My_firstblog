16a0cc2ea84101a1f403ce1cb20a93b90dc74d06
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-26 00:34:47 -0500

    Blog with HTML and CSS added

--- swethasblog/urls.py
+++ swethasblog/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'swethasblog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('swethasblog.urls')),
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
