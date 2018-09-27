cdae7e37069ca5fc378103dfc77cd39a2345bc50
swetha byluppala <sbyluppala@unomaha.edu>
2018-09-26 20:12:23 -0500

    updated comment section

--- swethasblog/admin.py
+++ swethasblog/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -16,3 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
